c790cea0209da2e4852c6cd6481eb11a745031d4
alexdelaroza <alex.delaroza@gmail.com>
2026-02-06 01:15:26 -0300

    insert

---
+++ sql/insert/insert.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@/cursogo")
+	if err != nil {
+		log.Fatal("Erro ao conectar:", err)
+	}
+	defer db.Close()
+
+	query := `INSERT INTO usuarios (
+                          cod_usuario, 
+						  nome_usuario, 
+						  login_usuario,
+						  senha_usuario, 
+						  email_usuario, 
+						  tipo_usuario
+                 ) VALUES (?, ?, ?, ?, ?, ?)`
+
+	stmt, _ := db.Prepare(query)
+
+	res, _ := stmt.Exec(1, // cod_usuario
+		"Maria",           // nome_usuario
+		"mariasilva",      // login_usuario
+		"senha123",        // senha_usuario
+		"maria@email.com", // email_usuario
+		"admin",           // tipo_usuario
+	)
+
+	id, _ := res.LastInsertId()
+	fmt.Println(id)
+
+	linhas, _ := res.RowsAffected()
+	fmt.Printf("Sucesso! %d linha(s) afetada(s).\n", linhas)
+
+}
