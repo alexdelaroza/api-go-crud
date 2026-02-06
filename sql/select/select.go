fa84fd1fbd0762f72b353c7b2d6bb2e85b8f9b9a
alexdelaroza <alex.delaroza@gmail.com>
2026-02-06 01:56:03 -0300

    sql

---
+++ sql/select/select.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type usuario struct {
+	cod_usuario   int
+	nome_usuario  string
+	login_usuario string
+	senha_usuario string
+	email_usuario string
+	tipo_usuario  string
+	data_ult_atu  time.Time
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@/cursogo")
+	if err != nil {
+		log.Fatal("Erro ao conectar:", err)
+	}
+	defer db.Close()
+
+	rows, _ := db.Query("select * from usuarios where cod_usuario = ?", 1)
+	defer db.Close()
+
+	for rows.Next() {
+		var u usuario
+		rows.Scan(&u.cod_usuario, &u.nome_usuario, &u.login_usuario, &u.senha_usuario, &u.email_usuario, &u.tipo_usuario, &u.data_ult_atu)
+		fmt.Println(u)
+	}
+
+}
