365802d93d00b93a26cb363efce4ff9693c786da
alexdelaroza <alex.delaroza@gmail.com>
2026-02-13 11:02:43 -0300

    SERVER

--- app/src/api/api.go
+++ app/src/api/api.go
@@ -6,45 +6,50 @@ import (
 
 // TESTE
 func Consulta_Usuario(c *fiber.Ctx) error {
-	return c.SendString("Hello C, World 👋!")
-}
-// TESTE
-func Insere_Usuario(c *fiber.Ctx) error {
-	return c.SendString("Hello R, World 👋!")
-}
-// TESTE
-func Atualiza_Usuario(c *fiber.Ctx) error {
-	return c.SendString("Hello R, World 👋!")
-}
-// TESTE
-func Deleta_Usuario(c *fiber.Ctx) error {
-	return c.SendString("Hello R, World 👋!")
+	return c.SendString("Hello Consulta_Usuario, World 👋!")
 }
 
+// TESTE
+func Insere_Usuario(c *fiber.Ctx) error {
+	return c.SendString("Hello Insere_Usuario, World 👋!")
+}
+
+// TESTE
+func Atualiza_Usuario(c *fiber.Ctx) error {
+	return c.SendString("Hello Atualiza_Usuario, World 👋!")
+}
+
+// TESTE
+func Deleta_Usuario(c *fiber.Ctx) error {
+	return c.SendString("Hello Deleta_Usuario, World 👋!")
+}
 
 // TESTE
 func Consulta_Servico(c *fiber.Ctx) error {
-	return c.SendString("Hello C, World 👋!")
-}
-// TESTE
-func Insere_Servico(c *fiber.Ctx) error {
-	return c.SendString("Hello R, World 👋!")
-}
-// TESTE
-func Atualiza_Servico(c *fiber.Ctx) error {
-	return c.SendString("Hello R, World 👋!")
-}
-// TESTE
-func Deleta_Servico(c *fiber.Ctx) error {
-	return c.SendString("Hello R, World 👋!")
+	return c.SendString("Hello Consulta_Servico, World 👋!")
 }
 
+// TESTE
+func Insere_Servico(c *fiber.Ctx) error {
+	return c.SendString("Hello Insere_Servico, World 👋!")
+}
+
+// TESTE
+func Atualiza_Servico(c *fiber.Ctx) error {
+	return c.SendString("Hello Atualiza_Servico, World 👋!")
+}
+
+// TESTE
+func Deleta_Servico(c *fiber.Ctx) error {
+	return c.SendString("Hello Deleta_Servico, World 👋!")
+}
 
 // TESTE
 func Consulta_Log(c *fiber.Ctx) error {
-	return c.SendString("Hello C, World 👋!")
+	return c.SendString("Hello Consulta_Log, World 👋!")
 }
+
 // TESTE
 func Insere_Log(c *fiber.Ctx) error {
-	return c.SendString("Hello R, World 👋!")
+	return c.SendString("Hello Insere_Log, World 👋!")
 }
