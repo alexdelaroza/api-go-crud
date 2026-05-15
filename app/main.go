d3bf2e118cc0cc89aea63bc4f08f9365cd42097e
alexdelaroza <alex.delaroza@gmail.com>
2026-05-15 16:24:28 -0300

    altera

--- app/main.go
+++ app/main.go
@@ -14,10 +14,8 @@ import (
 
 func main() {
 
-	// 1. Carregar as Variavei de Ambiente
 	config.CarregarConfig()
 
-	// 2. Conecta ao banco de dados
 	err := database.ConectarDb()
 	if err != nil {
 		log.Fatal("Erro fatal ao conectar no banco:", err)
@@ -26,16 +24,10 @@ func main() {
 
 	fmt.Println("Conexão com o banco estabelecida com sucesso!")
 
-	//Caso precise criar a estrutura do Banco de Dados
-	//database.Create_table()
-	
-	// 3. cria a instancia do WEB server
+	//cria a instancia do WEB server
 	app := fiber.New()
 
-	// CORS é uma medida de segurança para proteger os usuários contra vulnerabilidades e ataques maliciosos.
-	// AllowCredentials definida como true, permite que o servidor inclua cookies e cabeçalhos de autenticação na solicitação.
 	app.Use(cors.New(cors.Config{
-		// Permite tanto o acesso local quanto o acesso pelo IP da sua rede
 		AllowOrigins:     "http://localhost:3001, http://127.0.0.1:3001, http://192.168.31.14:3001",
 		AllowCredentials: true,
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
